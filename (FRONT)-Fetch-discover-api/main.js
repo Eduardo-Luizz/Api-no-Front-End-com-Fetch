3fc0dbd4eb88f0197d7f6ca76f20a84bfe4f1ac6
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-01 15:42:18 -0300

    Acertando descrição

--- (FRONT)-Fetch-discover-api/main.js
+++ (FRONT)-Fetch-discover-api/main.js
@@ -3,7 +3,7 @@ const url = "http://localhost:5500/api"
 function getUsers() {
   fetch(url) // Batendo na url
     .then( response => response.json()) // Se sucesso .then e retorna resposta
-    .then( data => renderApiResult.textContent = JSON.stringify(data)) // pegou o data q armazena os dados e pegou o id da dib html para mostrar no front (Convertido para JSON)
+    .then( data => renderApiResult.textContent = JSON.stringify(data)) // pegou o data que vem do response e armazena os dados e pegou o id da div html para mostrar no front (Convertido para JSON)
     .catch( error => console.error(error) );          // Se erro .catch e retorna erro
 }
 
